2b25c14291be24c5fd0486c4904976cc128af917
hmiwa30 <109275358+hmiwa30@users.noreply.github.com>
2022-07-15 18:31:47 +0900

    Update qpfstats_git.sh

--- qpfstats_git.sh
+++ qpfstats_git.sh
@@ -10,6 +10,5 @@
 #$ -o ~/hmiwa/log/qpfstats_git.o
 #$ -e ~/hmiwa/log/qpfstats_git.e
 echo "start: `date`"
-git pull origin main
 singularity exec /usr/local/biotools/a/admixtools:7.0.2--h767d0c5_0 qpfstats -p ~/hmiwa/admixtools-test/parqpfstat_git.txt;echo "qpfstats done: `date`"
 echo "finish: `date`"
